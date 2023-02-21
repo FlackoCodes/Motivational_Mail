082a3c5d49bef7313c927fed45604fdeab4d11c7
Joseph K. L Lartey <lesliejoe2k2@gmail.com>
2023-02-21 15:48:20 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 from datetime import datetime as dt
 from smtplib import SMTP
-import random 
+import random
+
+# Constants
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+SENDER_EMAIL = "your_mail"
+RECIPIENT_EMAIL = "recepient_mail"
+APP_PASSWORD = "your smtplib app password"
+SUBJECT = "Monday Motivation Mail"
+QUOTES_FILE = "quotes.txt"
+
+def send_email(sender_email, recipient_email, subject, message):
+    """Send an email with the given subject and message from the given sender to the given recipient on mondays."""
+    with SMTP(SMTP_SERVER, port=SMTP_PORT) as connection:
+        connection.starttls()
+        connection.login(user=sender_email, password=APP_PASSWORD)
+        connection.sendmail(from_addr=sender_email, to_addrs=recipient_email, msg=f"Subject: {subject}\n\n{message}")
+
 now = dt.now() # checking the current date and time
 
 # checking if the day is monday
-
-if now.weekday() == 1:
-    with open("quotes.txt") as quotes_file:
+if now.weekday() == 0:
+    with open(QUOTES_FILE) as quotes_file:
         all_quotes = quotes_file.readlines()
         quote = random.choice(all_quotes)
-        MY_EMAIL = "lesliejoe2k2@gmail.com"
-        RECIEVER_EMAIL = "larteyleslie@yahoo.com"
-        GMAIL_APP_PASSWORD = "lgswruirjhbtoejj"
-        GMAIL_SMPT = "smtp.gmail.com"
-        
-        with SMTP(GMAIL_SMPT, port=587) as connection:
-                    connection.starttls()
-                    connection.login(user=MY_EMAIL, password=GMAIL_APP_PASSWORD)
-                    connection.sendmail(from_addr=MY_EMAIL, to_addrs=RECIEVER_EMAIL, msg=f"Subject: Monday Motivation Mail\n\n {quote}")
-        
-else:
-    print("Yessir!")
-print(quote)
+        send_email(SENDER_EMAIL, RECIPIENT_EMAIL, SUBJECT, quote)
